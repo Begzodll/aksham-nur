85cba00d1b56ad4642799e7b6ee0bae28751f1e2
57073731sb <begzodsirojiddinov9@gmail.com>
2023-01-06 02:46:19 +0500

    home completed

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,15 @@
-import {BannerBackground,ShadowContainer} from './HomeStyle';
+import {BannerBackground, ShadowContainer, MainText, SubText, BtnGroup, BtnOrder} from './HomeStyle';
 import banner from '../../Assets/images/home-bg/v-3.jpg';
 const Home = () => {
     return(
         <BannerBackground src={banner}>
             <ShadowContainer>
-
+                <MainText>Aksham Nurga xush kelibsiz</MainText>
+                <SubText>Biz sizni kutayotgan edik <br/> Biz sizni shirin taom bilan jazo'laymiz</SubText>
+                <BtnGroup>
+                    <BtnOrder>Buyurtma berish</BtnOrder>
+                    <BtnOrder>Bo'glanish</BtnOrder>
+                </BtnGroup>
             </ShadowContainer>
         </BannerBackground>
     )
--- src/Pages/Home/HomeStyle.js
+++ src/Pages/Home/HomeStyle.js
@@ -13,5 +13,67 @@ export const ShadowContainer = styled.div`
   width: 100%;
   height: 100%;
   position: absolute;
-  background: rgba(0, 0, 0, 0.75);
+  background: rgba(0, 0, 0, 0.83);
+`
+export const MainText = styled.p`
+  color: #cbcaca;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 8rem;
+  font-size: 53px;
+  @media (max-width: 730px) {
+    font-size: 47px;
+  }
+  @media (max-width: 596px) {
+    font-size: 40px;
+  }
+  @media (max-width: 505px) {
+    font-size: 30px;
+  }
+  @media (max-width: 408px) {
+    font-size: 23px;
+  }
+`
+export const SubText = styled.p`
+  color: #cbcaca;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  font-size: 25px;
+  margin-top: 2rem;
+  @media (max-width: 505px) {
+    font-size: 22px;
+  }
+  @media (max-width: 408px) {
+    font-size: 16px;
+    margin-top: 1rem;
+  } 
+`
+export const BtnGroup = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  @media(max-width: 410px){
+    flex-direction: column;
+    margin-top: 2rem;
+  }
+`
+export const BtnOrder = styled.button`
+  padding: 8px 10px;
+  color: #FFFF00;
+  border: 1px solid #FFFF00;
+  transition: .5s all ease;
+  margin: 4rem 1rem;
+  width: 150px;
+
+  &:hover {
+    background: rgba(0, 0, 0, 0.6);
+    color: #cbcaca;
+    border: 1px solid #cbcaca;
+  }
+  @media(max-width: 410px){
+    margin: .6rem 0;
+  }
 `
